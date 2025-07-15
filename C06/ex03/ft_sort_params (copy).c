5094b4a9f4af98cb6b9cc538f995ee8c6c782e18
Nicolas Mayela <nmayela@f1r7s12.clusters.42paris.fr>
2025-07-15 19:17:35 +0200

    add ex03 finished and a version which sort according to the lenght

--- C06/ex03/ft_sort_params (copy).c
+++ C06/ex03/ft_sort_params (copy).c
@@ -6,7 +6,7 @@
 /*   By: nmayela <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 14:01:39 by nmayela           #+#    #+#             */
-/*   Updated: 2025/07/15 19:14:22 by nmayela          ###   ########.fr       */
+/*   Updated: 2025/07/15 19:05:32 by nmayela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -26,6 +26,53 @@ int	ft_strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+int	ft_strcmplen(char *str1, char *str2)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str1[i])
+	{
+		i++;
+	}
+	j = 0;
+	while (str2[j])
+	{
+		j++;
+	}
+	if (i >= j)
+		return (1);
+	return (0);
+}
+
+void	sortlen(char *tab[], int size)
+{
+	char	*min;
+	int		i;
+	int		j;
+
+	i = 1;
+	min = 0;
+	while (i < size)
+	{
+		min = tab[i];
+		j = i;
+		while (j < size)
+		{
+			if (ft_strcmplen(min, tab[j]))
+			{
+				min = tab[j];
+				tab[j] = tab[i];
+				tab[i] = min;
+			}
+			j++;
+		}
+		tab[i] = min;
+		i++;
+	}
+}
+
 void	sort(char *tab[], int size)
 {
 	char	*min;
@@ -40,7 +87,7 @@ void	sort(char *tab[], int size)
 		j = i;
 		while (j < size)
 		{
-			if ((ft_strcmp(min, tab[j]) >= 0))
+			if ((ft_strcmp(min, tab[j]) >= 0) && (ft_strcmplen(min, tab[j])))
 			{
 				min = tab[j];
 				tab[j] = tab[i];
@@ -60,6 +107,7 @@ int	main(int argc, char *argv[])
 
 	i = 1;
 	j = 0;
+	sortlen(argv, argc);
 	sort(argv, argc);
 	while (i < argc)
 	{
